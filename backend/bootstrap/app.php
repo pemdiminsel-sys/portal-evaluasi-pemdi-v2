763ca2146a298a4ac998e74482a51337dd797b96
steventeen <henceruindungan@gmail.com>
2026-04-07 14:48:04 +0800

    Fix: Enable CORS and CSRF exemption for API

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->statefulApi();
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
