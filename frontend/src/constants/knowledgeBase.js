dd384cf9e3e5f445e566b15c2226eaf3a8724364
steventeen <henceruindungan@gmail.com>
2026-04-10 00:33:12 +0800

    FIX: Build error due to syntax in knowledgeBase.js

--- frontend/src/constants/knowledgeBase.js
+++ frontend/src/constants/knowledgeBase.js
@@ -5,13 +5,13 @@ import { RAW_INDIKATOR } from './knowledge/raw_indikator';
 import { RAW_PERBUP } from './knowledge/raw_perbup';
 import { KNOWLEDGE_USER_MANUAL } from './knowledge/user_manual';
 
-export const KNOWLEDGE_BASE = \`
-\${KNOWLEDGE_REGULASI}
-\${KNOWLEDGE_INDIKATOR}
-\${KNOWLEDGE_OPERASIONAL}
-\${KNOWLEDGE_USER_MANUAL}
+export const KNOWLEDGE_BASE = `
+${KNOWLEDGE_REGULASI}
+${KNOWLEDGE_INDIKATOR}
+${KNOWLEDGE_OPERASIONAL}
+${KNOWLEDGE_USER_MANUAL}
 
 # DATA MENTAH LENGKAP (RAW DATABASE)
-\${RAW_PERBUP}
-\${RAW_INDIKATOR}
-\`;
+${RAW_PERBUP}
+${RAW_INDIKATOR}
+`;
